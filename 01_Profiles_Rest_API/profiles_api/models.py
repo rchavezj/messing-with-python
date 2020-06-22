a857a78b423d8fcf10ab9efc9e3e3a3a419e2027
Roberto Chavez Jr <rchavezj.ai@gmail.com>
2020-06-22 16:41:54 -0700

    Updated ReadME

--- 01_Profiles_Rest_API/profiles_api/models.py
+++ 01_Profiles_Rest_API/profiles_api/models.py
@@ -1,3 +1,24 @@
-from django.db import models
+# from django.db import models
+# from django.contrib.auth.models import AbstractBaseUser
+# from django.contrib.auth.models import PermissionsMixin
+# # Create your models here.
 
-# Create your models here.
+
+# class UserProfile(AbstractBaseUser, PermissionsMixin):
+#     """Database model for users in the system"""
+#     email = models.EmailField(max_length=255, unique=True)
+#     name = models.CharField(max_length=255)
+#     is_active = models.BooleanField(default=True)
+#     is_staff = models.BooleanField(default=False)
+
+#     objects = UserProfileManager()
+
+#     USERNAME_FIELD = 'email'
+#     REQUIRED_FIELDS = ['name']
+
+#     def get_full_name(self):
+#         """Retrieve full name of user"""
+#         return self.name
+
+#     def get_short_name(self):
+#         return self.name
